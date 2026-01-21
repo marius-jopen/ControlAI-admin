b004c67871d01ff3650d028d850b57747600cb3f
Marius Jopen <mail@mariusjopen.com>
2026-01-21 13:52:40 -0300

    added fal.ai

--- src/routes/admin/apps/+page.svelte
+++ src/routes/admin/apps/+page.svelte
@@ -409,6 +409,7 @@
     { id: 'open-image', name: 'OpenAI Image', loraType: null },
     { id: 'g-image', name: 'Google Image', loraType: null },
     { id: 'g-image-3', name: 'Google Image 3 Pro 🍌', loraType: null },
+    { id: 'g-image-3b', name: 'Google Image 3b (fal.ai) 🍌', loraType: null },
     { id: 'sdxl-i2i', name: 'SDXL Image-to-Image', loraType: 'sdxl' },
     { id: 'open-inpaint', name: 'OpenAI Inpaint', loraType: null },
     { id: 'deforum-basic', name: 'Deforum Basic', loraType: 'sdxl' },
